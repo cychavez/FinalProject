fe01cbae0bd8ada8726be6e348ccbdd883d3344a
Kevin Lanni <therealklanni@gmail.com>
2015-09-09 12:47:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+import path from 'path'
+import logger from 'morgan'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+import api from './routes/api/v1'
+
+app.use('/api/v1', api)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+module.exports = app
